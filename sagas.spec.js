fa8fd494518d512756d58c597fa1cc95f92a7088
Humenchuk Serhii <shumenchuk@varteq.com>
2020-01-02 18:35:40 +0200

    Create-react-app. Add redux-saga library. Connect saga to store. Create sync and async actions

---
+++ sagas.spec.js
@@ -0,0 +1,10 @@
+import test from 'tape';
+import { incrementAsync } from './sagas';
+
+test('incrementAsync Saga test', assert => {
+  const gen = incrementAsync();
+
+  // now what ?
+});
+
+gen.next();
